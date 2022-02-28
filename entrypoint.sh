fbf88ea332ce592b72bc91e0e15e95e3727380a4
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2022-02-28 10:14:46 -0500

    creación de punto de ingreso para ci

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+if [ -n "${PARAMETER_STORE:-}" ]; then
+  export CITAS_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/citas_crud/db/username --output text --query Parameter.Value)"
+  export CITAS_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/citas_crud/db/password --output text --query Parameter.Value)"
+fi
+
+exec ./main "$@"
